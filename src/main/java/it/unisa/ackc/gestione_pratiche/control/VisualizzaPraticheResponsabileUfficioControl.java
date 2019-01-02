ce93e1f988c0ed2c5a4ca20555cfef27abdfedf7
Giuseppe Grieco <email.grieco@gmail.com>
2019-01-02 16:52:48 +0100

    Coding VisualizzaPraticheResponsabileUfficioControl, VisualizzaPraticheStudenteControl

--- src/main/java/it/unisa/ackc/gestione_pratiche/control/VisualizzaPraticheResponsabileUfficioControl.java
+++ src/main/java/it/unisa/ackc/gestione_pratiche/control/VisualizzaPraticheResponsabileUfficioControl.java
@@ -1,25 +1,84 @@
 package it.unisa.ackc.gestione_pratiche.control;
 
+import it.unisa.ackc.HttpServletWithCheck;
+import it.unisa.ackc.gestione_pratiche.entity.Pratica;
+import it.unisa.ackc.gestione_storage.ejb.ACKStorageFacadeEJB;
+import it.unisa.ackc.gestione_utenti.entity.Account;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Si occupa della visualizzazione e filtraggio delle pratiche per un responsabile ufficio.
  *
  * @version 0.0.1
  */
-public class VisualizzaPraticheResponsabileUfficioControl extends HttpServlet {
+@WebServlet("/gestione-pratiche/visualizza-pratiche-responsabile-ufficio")
+public class VisualizzaPraticheResponsabileUfficioControl extends HttpServletWithCheck {
+    private static final String FILTRO_PARAMETRO = "filtro";
+    private static final String PAGINA_PARAMETRO = "pagina";
+    private static final String PRATICHE_JSP = "";
+    private static final int NESSUN_FILTRO = 0;
+    private static final int PRATICHE_DA_VALUTARE = 1;
+    private static final int PRATICHE_SOSPESE = 2;
+    private static final int PRATICHE_CHIUSE = 3;
+
     /**
-     * Recupera le informazioni (opportunamente filtrate) relative alla lista di pratiche per
-     * un responsabile ufficio.
+     * Istanza dello storage facade.
+     */
+    @Inject
+    private ACKStorageFacadeEJB ackStorage;
+
+    /**
+     * Recupera le informazioni (opportunamente filtrate) relative alla
+     * lista di pratiche per un responsabile ufficio.
      *
      * @since 0.0.1
      */
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    public void doGet(
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws ServletException, IOException {
+        valida(request);
+        Account account = (Account)
+                request.getSession().getAttribute("account");
+        int filtro = Integer.parseInt(request.getParameter(FILTRO_PARAMETRO));
+        int pagina = Integer.parseInt(request.getParameter(PAGINA_PARAMETRO));
+        List<Pratica> praticheResponsabileUfficio = null;
+        switch (filtro) {
+            case PRATICHE_SOSPESE:
+                //TODO praticheStudente = ACKStorageFacade.getPraticheByResponsabileUfficioSospese(account, pagina)
+                break;
+            case PRATICHE_DA_VALUTARE:
+                //TODO praticheStudente = ACKStorageFacade.getPraticheByResponsabileUfficioDaValutare(account, pagina)
+                break;
+            case PRATICHE_CHIUSE:
+                //TODO praticheStudente = ACKStorageFacade.getPraticheByResponsabileUfficioChiuse(account, pagina)
+                break;
+            case NESSUN_FILTRO:
+            default:
+                //TODO praticheStudente = ACKStorageFacade.getPraticheByResponsabileUfficio(account, pagina)
+        }
+        request.setAttribute("pratiche", praticheResponsabileUfficio);
+        request.getRequestDispatcher(PRATICHE_JSP).forward(request, response);
+    }
+
+    /**
+     * Valida i parametri della richiesta.
+     *
+     * @param request contenente i parametri da validare
+     * @since 0.0.1
+     */
+    @Override
+    public void valida(final HttpServletRequest request) {
+        //TODO aggiungi convalida
+        super.valida(request);
     }
 }
--- src/main/java/it/unisa/ackc/gestione_pratiche/control/VisualizzaPraticheStudenteControl.java
+++ src/main/java/it/unisa/ackc/gestione_pratiche/control/VisualizzaPraticheStudenteControl.java
@@ -1,24 +1,63 @@
 package it.unisa.ackc.gestione_pratiche.control;
 
+import it.unisa.ackc.HttpServletWithCheck;
+import it.unisa.ackc.gestione_pratiche.entity.Pratica;
+import it.unisa.ackc.gestione_storage.ejb.ACKStorageFacadeEJB;
+import it.unisa.ackc.gestione_utenti.entity.Account;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Si occupa della visualizzazione delle pratiche per uno studente.
  *
  * @version 0.0.1
  */
-public class VisualizzaPraticheStudenteControl extends HttpServlet {
+@WebServlet("/gestione-pratiche/visualizza-pratiche-studente")
+public class VisualizzaPraticheStudenteControl extends HttpServletWithCheck {
+    private static final String PRATICHE_JSP = "";
+    private static final String PAGINA_PARAMETRO = "pagina";
+
+    /**
+     * Istanza dello storage facade.
+     */
+    @Inject
+    private ACKStorageFacadeEJB ackStorage;
+
     /**
      * Recupera le informazioni relative alla lista di pratiche per uno studente.
      *
      * @since 0.0.1
      */
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    public void doGet(
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws ServletException, IOException {
+        Account account = (Account)
+                request.getSession().getAttribute("account");
+        int pagina = Integer.parseInt(request.getParameter(PAGINA_PARAMETRO));
+        //TODO controllo sui dati
+        List<Pratica> praticheStudente = null;
+        //TODO praticheStudente = ACKStorageFacade.getPraticheByStudente(account, pagina)
+        request.setAttribute("pratiche", praticheStudente);
+        request.getRequestDispatcher(PRATICHE_JSP).forward(request, response);
+    }
+
+    /**
+     * Valida i parametri della richiesta.
+     *
+     * @param request contenente i parametri da validare
+     * @since 0.0.1
+     */
+    @Override
+    public void valida(final HttpServletRequest request) {
+        //TODO aggiungi convalida
+        super.valida(request);
     }
 }
