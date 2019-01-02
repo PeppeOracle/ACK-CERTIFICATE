140bb542a99a322279281b4ae2ff4431b2d2b03f
Giuseppe Grieco <email.grieco@gmail.com>
2019-01-02 22:04:11 +0100

    Coding validator e aggiunta DomandaLinguaInglese.pdf

--- src/main/java/it/unisa/ackc/validator/CondizioneConvalida.java
+++ src/main/java/it/unisa/ackc/validator/CondizioneConvalida.java
@@ -2,6 +2,15 @@ package it.unisa.ackc.validator;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Contiene la logica di una condizione di convalida.
+ */
 public interface CondizioneConvalida {
+    /**
+     * Convalida la condizione.
+     *
+     * @param request da validare
+     * @return notifica contenente la collezione di errori trovati
+     */
     Notifica validate(HttpServletRequest request);
 }
---
+++ src/main/java/it/unisa/ackc/validator/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Fornisce le interfacce e le classi necessari per il
+ * rilevamento e la comunicazione degli errori.
+ */
+package it.unisa.ackc.validator;
