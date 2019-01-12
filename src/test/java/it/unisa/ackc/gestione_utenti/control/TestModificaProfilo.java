a31c9bf88fadca95fea5bf7c823b076ce3dd9017
Vincenzo <v.santoro21@studenti.unisa.it>
2019-01-12 14:43:59 +0100

    Test Modifica Profilo
    incompleto

--- src/test/java/it/unisa/ackc/gestione_utenti/control/TestModificaProfilo.java
+++ src/test/java/it/unisa/ackc/gestione_utenti/control/TestModificaProfilo.java
@@ -1,1 +1,18 @@
+package it.unisa.ackc.gestione_utenti.control;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestModificaProfilo {
+    //da fare
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test01(){
+        
+    }
+}
