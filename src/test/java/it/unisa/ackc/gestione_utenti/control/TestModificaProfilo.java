2138edff926530b83277569e8462c9c8aa22c881
Vincenzo <v.santoro21@studenti.unisa.it>
2019-01-12 17:31:34 +0100

    Update TestModificaProfilo.java
    il codice dà errore, non utilizzate questa classe finchè non l'aggiorno

--- src/test/java/it/unisa/ackc/gestione_utenti/control/TestModificaProfilo.java
+++ src/test/java/it/unisa/ackc/gestione_utenti/control/TestModificaProfilo.java
@@ -1,18 +1,26 @@
 package it.unisa.ackc.gestione_utenti.control;
 
+import it.unisa.ackc.gestione_utenti.control.ModificaProfiloStudente;
+import it.unisa.ackc.http.stub.RispostaStub;
+import it.unisa.ackc.http.stub.SessioneStub;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class TestModificaProfilo {
-    //da fare
+
+    private ModificaProfiloStudente modificaProfiloStudente;
+    private SessioneStub sessioneStub;
+    private RispostaStub rispostaStub;
+
     @Before
     public void setUp() throws Exception {
+        sessioneStub = new SessioneStub();
+        rispostaStub = new RispostaStub();
+        modificaProfiloStudente = new ModificaProfiloStudente(sessioneStub, rispostaStub);
     }
 
     @Test
     public void test01(){
-        
+
     }
 }
