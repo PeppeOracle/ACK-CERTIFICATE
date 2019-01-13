6abda60428db46bfd641d8cf9d4d6b318d223048
Vincenzo <v.santoro21@studenti.unisa.it>
2019-01-13 11:38:03 +0100

    Aggiunti 7 Test

--- src/test/java/it/unisa/ackc/gestione_utenti/control/ModificaProfiloStudenteTest.java
+++ src/test/java/it/unisa/ackc/gestione_utenti/control/ModificaProfiloStudenteTest.java
@@ -2,6 +2,7 @@ package it.unisa.ackc.gestione_utenti.control;
 
 import it.unisa.ackc.form.FormDati;
 import it.unisa.ackc.gestione_utenti.control.convalida.AccountConvalida;
+import it.unisa.ackc.gestione_utenti.control.convalida.AccountStudente;
 import it.unisa.ackc.http.stub.RispostaStub;
 import it.unisa.ackc.http.stub.SessioneStub;
 import org.junit.Before;
@@ -15,6 +16,7 @@ public class ModificaProfiloStudenteTest {
     private SessioneStub sessioneStub;
     private RispostaStub rispostaStub;
     private FormDati formDati;
+    private String longString;
 
     @Before
     public void setUp() throws Exception {
@@ -22,9 +24,13 @@ public class ModificaProfiloStudenteTest {
         rispostaStub = new RispostaStub();
         modificaProfiloStudente = new ModificaProfiloStudente(sessioneStub, rispostaStub);
         formDati = new FormDati();
+        longString = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
         formDati.aggiungiDato(AccountConvalida.NOME_PARAMETRO, "Mario");
-        // TODO aggiungere gli altri parametri
+        formDati.aggiungiDato(AccountConvalida.COGNOME_PARAMETRO, "Rossi");
+        formDati.aggiungiDato(AccountStudente.LUOGO_DI_NASCITA_PARAMETRO, "Avellino");
+
+        //modificaProfiloStudente.valida(formDati);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -32,4 +38,47 @@ public class ModificaProfiloStudenteTest {
         formDati.aggiungiDato(AccountConvalida.NOME_PARAMETRO, "");
         modificaProfiloStudente.valida(formDati);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test02(){
+        formDati.aggiungiDato(AccountConvalida.NOME_PARAMETRO, longString);
+        modificaProfiloStudente.valida(formDati);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test03(){
+        formDati.aggiungiDato(AccountConvalida.NOME_PARAMETRO, "Mario!");
+        modificaProfiloStudente.valida(formDati);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test04(){
+        formDati.aggiungiDato(AccountConvalida.COGNOME_PARAMETRO, "");
+        modificaProfiloStudente.valida(formDati);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test05(){
+        formDati.aggiungiDato(AccountConvalida.COGNOME_PARAMETRO, longString);
+        modificaProfiloStudente.valida(formDati);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test06(){
+        formDati.aggiungiDato(AccountConvalida.COGNOME_PARAMETRO, "Rossi?");
+        modificaProfiloStudente.valida(formDati);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test07(){
+        formDati.aggiungiDato(AccountStudente.LUOGO_DI_NASCITA_PARAMETRO, "");
+        modificaProfiloStudente.valida(formDati);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test08(){
+        formDati.aggiungiDato(AccountStudente.LUOGO_DI_NASCITA_PARAMETRO, longString);
+        modificaProfiloStudente.valida(formDati);
+    }
+    
 }
