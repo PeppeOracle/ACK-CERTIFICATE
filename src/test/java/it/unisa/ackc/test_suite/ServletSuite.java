9a5eee39fe4816e6360fca6fd7f4ad2f175324f9
Giuseppe Ambrosio <g.ambrosio25@studenti.unisa.it>
2019-01-19 19:49:22 +0100

    testing

--- src/test/java/it/unisa/ackc/test_suite/ServletSuite.java
+++ src/test/java/it/unisa/ackc/test_suite/ServletSuite.java
@@ -12,7 +12,8 @@ import org.junit.runners.Suite;
         CreazioneDomandaLinguaIngleseTest.class,
         CreazionePraticaTest.class,
         AutenticazioneUtenteTest.class,
-        RegistrazioneAccountResponsabileUfficioTest.class
+        RegistrazioneAccountResponsabileUfficioTest.class,
+        MostraPraticaTest.class
 })
 public class ServletSuite {
 }
